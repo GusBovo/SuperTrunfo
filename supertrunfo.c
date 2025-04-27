aae2d9ee4c17313ed6773e457fb962c0a718543c
Gustavo Bovo <gustavobovo1995@gmail.com>
2025-04-27 18:03:33 -0300

    Cria supertrunfo.c
    Adiciona declaração inicial de variáveis.

---
+++ supertrunfo.c
@@ -0,0 +1,14 @@
+int main()
+{
+
+  char estados;
+  char codigo_carta[3];
+  char nome_cidade[30];
+  int populacao;
+  float area;
+  float pib;
+  int num_pontos_turisticos;
+
+  
+  return 1;
+}
